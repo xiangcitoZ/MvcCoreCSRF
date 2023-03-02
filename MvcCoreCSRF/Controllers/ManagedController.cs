6725f640b2a58c2678713f4ca87bb91a011c2fd5
xiangcitoZ <pzxiang1@gmail.com>
2023-03-02 11:23:34 +0100

    Add project files.

---
+++ MvcCoreCSRF/Controllers/ManagedController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreCSRF.Controllers
+{
+    public class ManagedController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string usuario, string password)
+        {
+            if (usuario.ToLower() == "admin" && password.ToLower() == "admin")
+            {
+                //ALMACENAMOS EL USUARIO EN SESSION
+                HttpContext.Session.SetString("USUARIO", usuario);
+                return RedirectToAction("Productos", "Tienda");
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Usuario/Password incorrectos";
+                return View();
+            }
+        }
+
+        public IActionResult AccesoDenegado()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ MvcCoreCSRF/Controllers/TiendaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreCSRF.Controllers
+{
+    public class TiendaController : Controller
+    {
+        public IActionResult Productos()
+        {
+            if (HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("AccesoDenegado", "Managed");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Productos(string direccion
+            , string[] producto)
+        {
+            if (HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("AccesoDenegado", "Managed");
+            }
+            else
+            {
+                TempData["PRODUCTOS"] = producto;
+                TempData["DIRECCION"] = direccion;
+                return RedirectToAction("PedidoFinal");
+            }
+        }
+
+        public IActionResult PedidoFinal()
+        {
+            string[] productos = TempData["PRODUCTOS"] as string[];
+            ViewData["DIRECCION"] = TempData["DIRECCION"].ToString();
+            return View(productos);
+        }
+
+    }
+}
